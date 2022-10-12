68fe330bea8aa93a8c39929a112032e24fba9de0
Tori <victorialuce34@gmail.com>
2022-10-12 10:50:22 -0500

    Display json message from server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import { useState, useEffect } from "react";
+
+
+const getPing = async () => {
+	try {
+		const res = await fetch("http://localhost:3001/ping", {
+			method: "GET",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+		});
+
+		return await res.json();
+	} catch (err) {}
+};
 
 function App() {
+  const [data, setData] = useState("Hello World!");
+
+	useEffect(() => {
+		getPing()
+			.then((res) => setData(res.message))
+			.catch((err) => console.log(err));
+	}, []);
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>{data}</h1>
     </div>
   );
 }
