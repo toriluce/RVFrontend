7f6193ddf2631d2005da2389ceaf7156fd39bd27
Victoria Luce <tori@Brendans-MacBook-Air.local>
2023-01-05 18:39:10 -0600

    Add backend api gateway url

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const URL = "http://localhost:3001"
+export const URL = process.env.REACT_APP_LOCAL ? "http://localhost:3001" : "https://9s6uud8jc0.execute-api.us-east-1.amazonaws.com/prod"
 export const HEADERS = {
 	Accept: "application/json",
 	"Content-Type": "application/json",
